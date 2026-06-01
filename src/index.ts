957c006ac47afd2bda28cfcc76fa11c5ce3f1c86
dave-atw <dave.havens@alltheword.org>
2026-06-01 15:24:30 -0500

    Get TNN resources from Aquifer API instead of hardcoding

--- src/index.ts
+++ src/index.ts
@@ -23,11 +23,107 @@ async function askLlm(prompt: string, systemPrompt: string = '', responseJsonSch
 	return response.text
 }
 
+// aquifer utilities
+
+async function aquiferFetch(apiPath: string, queryParams: URLSearchParams): Promise<any> {
+	return fetch(`https://api.aquifer.bible${apiPath}?${queryParams.toString()}`, {
+		headers: {
+			"api-key": process.env.API_KEY_AQUIFER,
+		},
+	})
+}
+
+async function getAquiferContentIds(params): Promise<number[]> {
+	const queryParams = new URLSearchParams({
+		languageCode: 'eng',
+		resourceCollectionCode: 'SILOpenTranslatorsNotes',
+		bookCode: usfmBookCodeByBookName.filter(x => x.key == params.bookName)[0].value,
+		startChapter: params.chapterNo,
+		endChapter: params.chapterNo,
+		startVerse: params.verseNo,
+		endVerse: params.verseNo
+	})
+	const response = await aquiferFetch('/resources/search', queryParams)
+	return (await response.json()).items.map(({id}) => id)
+}
+
+// usfm utilities
+
+const usfmBookCodeByBookName = [
+	{ key: 'Genesis', value: 'GEN' },
+	{ key: 'Exodus', value: 'EXO' },
+	{ key: 'Leviticus', value: 'LEV' },
+	{ key: 'Numbers', value: 'NUM' },
+	{ key: 'Deuteronomy', value: 'DEU' },
+	{ key: 'Joshua', value: 'JOS' },
+	{ key: 'Judges', value: 'JDG' },
+	{ key: 'Ruth', value: 'RUT' },
+	{ key: '1 Samuel', value: '1SA' },
+	{ key: '2 Samuel', value: '2SA' },
+	{ key: '1 Kings', value: '1KI' },
+	{ key: '2 Kings', value: '2KI' },
+	{ key: '1 Chronicles', value: '1CH' },
+	{ key: '2 Chronicles', value: '2CH' },
+	{ key: 'Ezra', value: 'EZR' },
+	{ key: 'Nehemiah', value: 'NEH' },
+	{ key: 'Esther', value: 'EST' },
+	{ key: 'Job', value: 'JOB' },
+	{ key: 'Psalms', value: 'PSA' },
+	{ key: 'Proverbs', value: 'PRO' },
+	{ key: 'Ecclesiastes', value: 'ECC' },
+	{ key: 'Song of Songs', value: 'SNG' },
+	{ key: 'Isaiah', value: 'ISA' },
+	{ key: 'Jeremiah', value: 'JER' },
+	{ key: 'Lamentations', value: 'LAM' },
+	{ key: 'Ezekiel', value: 'EZK' },
+	{ key: 'Daniel', value: 'DAN' },
+	{ key: 'Hosea', value: 'HOS' },
+	{ key: 'Joel', value: 'JOL' },
+	{ key: 'Amos', value: 'AMO' },
+	{ key: 'Obadiah', value: 'OBA' },
+	{ key: 'Jonah', value: 'JON' },
+	{ key: 'Micah', value: 'MIC' },
+	{ key: 'Nahum', value: 'NAM' },
+	{ key: 'Habakkuk', value: 'HAB' },
+	{ key: 'Zephaniah', value: 'ZEP' },
+	{ key: 'Haggai', value: 'HAG' },
+	{ key: 'Zechariah', value: 'ZEC' },
+	{ key: 'Malachi', value: 'MAL' },
+
+	{ key: 'Matthew', value: 'MAT' },
+	{ key: 'Mark', value: 'MRK' },
+	{ key: 'Luke', value: 'LUK' },
+	{ key: 'John', value: 'JHN' },
+	{ key: 'Acts', value: 'ACT' },
+	{ key: 'Romans', value: 'ROM' },
+	{ key: '1 Corinthians', value: '1CO' },
+	{ key: '2 Corinthians', value: '2CO' },
+	{ key: 'Galatians', value: 'GAL' },
+	{ key: 'Ephesians', value: 'EPH' },
+	{ key: 'Philippians', value: 'PHP' },
+	{ key: 'Colossians', value: 'COL' },
+	{ key: '1 Thessalonians', value: '1TH' },
+	{ key: '2 Thessalonians', value: '2TH' },
+	{ key: '1 Timothy', value: '1TI' },
+	{ key: '2 Timothy', value: '2TI' },
+	{ key: 'Titus', value: 'TIT' },
+	{ key: 'Philemon', value: 'PHM' },
+	{ key: 'Hebrews', value: 'HEB' },
+	{ key: 'James', value: 'JAS' },
+	{ key: '1 Peter', value: '1PE' },
+	{ key: '2 Peter', value: '2PE' },
+	{ key: '1 John', value: '1JN' },
+	{ key: '2 John', value: '2JN' },
+	{ key: '3 John', value: '3JN' },
+	{ key: 'Jude', value: 'JUD' },
+	{ key: 'Revelation', value: 'REV' }
+];
+
 // content access
 
 async function getSource(params): Promise<string> {
-	const result = await fetch(`https://targets.tabitha.bible/${params.lwcName}/${params.bookName}/${params.chapterNo}/${params.verseNo}`)
-	const json = await result.json()
+	const response = await fetch(`https://targets.tabitha.bible/${params.lwcName}/${params.bookName}/${params.chapterNo}/${params.verseNo}`)
+	const json = await response.json()
 	return json.filter(({ audience, text }) => { return audience == 'Unchurched Adults' })[0].text
 }
 
@@ -41,15 +137,21 @@ async function getQuestions(params): Promise<string[]> {
 			show_note_sources: params.showNoteSources
 		}),
 	})
-	const result = await fetch(`https://copilot-dev.tabitha.bible/${params.bookName}/${params.chapterNo}/${params.verseNo}?${queryParams.toString()}`,
+	const response = await fetch(`https://copilot-dev.tabitha.bible/${params.bookName}/${params.chapterNo}/${params.verseNo}?${queryParams.toString()}`,
 	)
-	const json = await result.json()
+	const json = await response.json()
 	return json.cautions.map(({ note, source }) => note)
 }
 
 async function getTnnBasedInfo(params): Promise<any> {
-	// TODO: get prompt from TNN's API instead
-	const prompt = await Bun.file('prompts/tnn-hardcoded.txt').text()
+	// get prompt from Aquifer
+	const contentId = (await getAquiferContentIds(params))[0]
+	const response = await fetch(`https://api.aquifer.bible/resources/${contentId}`, {
+		headers: {
+			"api-key": process.env.API_KEY_AQUIFER,
+		},
+	})
+	const prompt = await response.text()
 
 	const systemPrompt = await Bun.file('prompts/tnn-system.txt').text()
 	const responseJsonSchema = {
